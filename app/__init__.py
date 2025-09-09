dffd62d452bce0801fa8c42b6e412a4055a0d502
Goitseone Rakgomo <119797578+ily6ix@users.noreply.github.com>
2025-09-09 09:34:34 +0200

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,10 @@
 from flask import Flask
-from .routes import main_bp
-from .config import Config
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
 
 def create_app():
-    app = Flask(__name__, static_folder="static", template_folder="templates")
-    app.config.from_object(Config)
-    db.init_app(app)
+    app = Flask(__name__)
 
-    app.register_blueprint(main_bp)
+    @app.route("/")
+    def home():
+        return "🚀 Hello from Flask on Vercel!"
 
     return app
